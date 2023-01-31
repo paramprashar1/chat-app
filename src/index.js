75ee1447505e36f53088d08d8f58c0138f5aeae1
paramprashar1 <param.prashar24@gmail.com>
2023-01-31 13:16:58 +0530

    Authentication

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//Avoid Multiple tabs
+const channel = new BroadcastChannel("tab");
+let isOriginal = true;
+
+channel.postMessage("another-tab");
+// note that listener is added after posting the message
+
+channel.addEventListener("message", (msg) => {
+  if (msg.data === "another-tab" && isOriginal) {
+    // message received from 2nd tab
+    // reply to all new tabs that the website is already open
+    channel.postMessage("already-open");
+  }
+  if (msg.data === "already-open") {
+    isOriginal = false;
+    // message received from original tab
+    // replace this with whatever logic you need
+    alert("Please navigate to the already open tab!");
+    window.open("about:blank", "_self");
+    window.close();
+  }
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-o2sn54rvs8m3hggi.jp.auth0.com"
+    clientId="8fHNrZlZNrGtTWcBK1ZWBQYoPZvnxGc8"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
